1b146f8c699cd6f2ba84e61d251e9601bcb52805
Gean Michel Ceretta <geanceretta@gmail.com>
2019-09-30 02:37:37 -0300

    Functional version, time measure based on CPU time

--- pi-series.c
+++ pi-series.c
@@ -4,83 +4,120 @@
 #include <pthread.h>
 #include <time.h>
 
-#define number_of_iterations 9000000000
+#define number_of_iterations 9000000000 //target
+//#define number_of_iterations 176 //=16*11 max of iterations where all 16 slices have a non zero value for method 1
+//#define number_of_iterations 368 //max of iterations where all 16 slices have a non zero value for method 2
 
 //David H Bailey reference article: https://www.davidhbailey.com/dhbpapers/pi-formulas.pdf
 
-void calculate_pi();
-void method_1();
-void method_2();
+long double calculate_pi();
+void* method_1();
+void* method_2();
+
+int number_of_threads[] = {1, 2, 4, 8, 16};
+double slice_size;
+long double partials[16]; //TODO leght of partial must be max of number_of_threads[]
+
 
 int main() {
 	
-	int i, method_counter, number_of_threads[] = {1, 2, 4, 8, 16};
-	int sets_of_threads = sizeof(number_of_threads) / sizeof(number_of_threads[0]);
-	int number_of_methods = 2;
+	int i, method_counter, number_of_methods = 2;
 	
 	clock_t start_cycle, end_cycle;
 	double cpu_time_used;
 	
-	double pi = 0;
+	long double pi = 0;
 	
-	for (method_counter = 0; method_counter < number_of_methods; method_counter) {
-		for (i = 0; i < sets_of_threads; i++) { //TODO:
+	int sets_of_threads = sizeof(number_of_threads) / sizeof(number_of_threads[0]); //number of elements in number_of_threads
+	
+	for (method_counter = 1; method_counter <= number_of_methods; method_counter++) {
+		for (i = 0; i < sets_of_threads; i++) {
 			
 			start_cycle = clock();
 			pi = calculate_pi(number_of_threads[i], method_counter);
 			end_cycle = clock();
 			
-			cpu_time_used = (end_cycle - start_cycle)/CLOCKS_PER_SEC;
+			cpu_time_used = (double)(end_cycle - start_cycle)/CLOCKS_PER_SEC;
 			
-			printf("\nFor %d threads, elapsed time was: %fs", i, cpu_time_used);
+			printf("\nFor %d threads, method %d, elapsed time was: %fs", number_of_threads[i], method_counter, cpu_time_used);
+			printf("\nValue of PI using method %d and %d threads: %.*Lf", method_counter, number_of_threads[i], 63, pi);
 		}
-	
-		printf("Value of PI using method %d: %f", method_counter, pi);
 	}
+	printf("\n");
 }
 
-void calculate_pi(int number_of_threads, int method) {
+long double calculate_pi(int number_of_threads, int method) {
 	
-	int thread_counter, rc;
-	double partial[threads], pi;
-	double initial_term, final_term, slice_size;
+	long thread_counter;
+	int rc, thread_exit_code = 0;
+	long double pi = 0;
+	double initial_term, final_term;
 	
 	pthread_t threads[number_of_threads];
 	
 	slice_size = number_of_iterations/number_of_threads;
 	
-	for (thread_counter = 0; thread_counter <= number_of_threads; thread_counter++) {
+	for (thread_counter = 0; thread_counter < number_of_threads; thread_counter++) {
 		
 		initial_term = thread_counter * slice_size;
 		final_term = (thread_counter + 1) * slice_size;
 		
-		if (method == 0) { // #13 series from David H. Bailey reference article
-			rc = pthread_create(&threads[thread_counter], NULL, method_1(initial_term, final_term, *partial[thread_counter]), NULL);
+		if (method == 1) { // #13 series from David H. Bailey reference article
+			rc = pthread_create(&threads[thread_counter], NULL, method_1, (void*)thread_counter);
 			if (rc) {
-				printf("/nFailed to create thread!");
+				printf("\nFailed to create thread!");
 			}
-		} else if (method == 1) { // #15 series from David H. Bailey reference article
-			rc = pthread_create(&threads[thread_counter], NULL, method_2(initial_term, final_term, *partial[thread_counter]), NULL);
+		} else if (method == 2) { // #15 series from David H. Bailey reference article
+			rc = pthread_create(&threads[thread_counter], NULL, method_2, (void*)thread_counter);
 			if (rc) {
-				printf("/nFailed to create thread!");
+				printf("\nFailed to create thread!");
 			}
 		}
 	}
 	
-	for (thread_counter = 0; thread_counter <= number_of_threads; thread_counter++) { //suspend until all threads finish
-		pthread_join(threads[thread_counter], (void**)&(NULL));
+	for (thread_counter = 0; thread_counter < number_of_threads; thread_counter++) { //suspend until all threads finish
+		rc = pthread_join(threads[thread_counter], (void**)&thread_exit_code);
+		if (rc) {
+			printf("\nFailed to join thread %ld", thread_counter);
+		}
 	}
-	
-	for (thread_counter = 0; thread_counter <= number_of_threads; thread_counter++) { //sum all partials of pi
-		pi = pi + partial[thread_counter];
+	for (thread_counter = 0; thread_counter < number_of_threads; thread_counter++) { //sum all partials of pi
+		//printf("\nSumming up partial of slice %ld: %.*Lf", thread_counter, 200, partials[thread_counter]);
+		pi += partials[thread_counter];
 	}
+	//printf("\nValue of Pi using %d threads: %.*Lf", number_of_threads, 50, pi);
+	return(pi);
 	
 }
 
-void method_1(double initial_term, double final_term, double *result) { // #13 series from David H. Bailey reference article
-	//TODO Implement #13 formula
+void* method_1(void* void_slice) { // #13 series from David H. Bailey reference article
+	long slice;
+	slice = (long)void_slice;
+	double initial_term = slice * slice_size;
+	double final_term = ((slice + 1) * slice_size) - 1;
+	//printf("\nSlice: %ld, slice size: %f, initial term: %f, final term: %f", slice, slice_size, initial_term, final_term);
+	double n;
+	long double partial = 0;
+	for (n = initial_term; n < final_term; n++) {
+		partial += (1/pow(16,n))*(4/(8*n + 1) - 2/(8*n + 4) - 1/(8*n + 5) - 1/(8*n + 6));
+	}
+	partials[slice] = partial;
+	//printf("\nPartial of slice %ld: %.*Lf", slice, 50, partial);
+	pthread_exit(NULL);
 }
 
-void method_2(double initial_term, double final_term, double *result) { // #15 series from David H. Bailey reference article
-	//TODO Implement #15 formula
+void* method_2(void* void_slice) { // #15 series from David H. Bailey reference article
+	long slice;
+	slice = (long)void_slice;
+	double initial_term = slice * slice_size;
+	double final_term = ((slice + 1) * slice_size) - 1;
+	//printf("\nSlice: %ld, slice size: %f, initial term: %f, final term: %f", slice, slice_size, initial_term, final_term);
+	double n;
+	long double partial = 0;
+	for (n = initial_term; n < final_term; n++) {
+		partial += (pow((-1),n)/pow(4,n))*(2/(4*n + 1) + 2/(4*n + 2) + 1/(4*n + 3));
+	}
+	partials[slice] = partial;
+	//printf("\nPartial of slice %ld: %.*Lf", slice, 50, partial);
+	pthread_exit(NULL);
 }
